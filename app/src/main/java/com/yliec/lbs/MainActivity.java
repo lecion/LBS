866e7605ec8f54e40fb607539dc44ae7e910ffad
lecion <ylc931116@gmail.com>
2015-04-22 03:25:02 +0800

    启用定位

--- app/src/main/java/com/yliec/lbs/MainActivity.java
+++ app/src/main/java/com/yliec/lbs/MainActivity.java
@@ -6,8 +6,12 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -15,14 +19,17 @@ public class MainActivity extends ActionBarActivity {
 
     private BaiduMap baiduMap;
 
+    private LocationClient locationClient;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mapView = (MapView) findViewById(R.id.bmapView);
         baiduMap = mapView.getMap();
-        baiduMap.setMapType(BaiduMap.MAP_TYPE_SATELLITE);
-        baiduMap.setTrafficEnabled(true);
+//        baiduMap.setMapType(BaiduMap.MAP_TYPE_SATELLITE);
+//        baiduMap.setTrafficEnabled(true);
 
     }
 
@@ -69,6 +76,20 @@ public class MainActivity extends ActionBarActivity {
     protected void onDestroy() {
         super.onDestroy();
         mapView.onDestroy();
+    }
+
+    public class MyLocationListener implements BDLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation bdLocation) {
+            if (bdLocation == null || mapView == null) {
+                return;
+            }
+            MyLocationData locationData = new MyLocationData.Builder().accuracy(bdLocation.getRadius())
+                    .direction(13f).latitude(bdLocation.getLatitude())
+                    .longitude(bdLocation.getLongitude()).build();
+            baiduMap.setMyLocationData(locationData);
+        }
     }
 
 
