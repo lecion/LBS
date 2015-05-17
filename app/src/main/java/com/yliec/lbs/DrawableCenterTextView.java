f5f842c2c817b1decdd4a92e919c23b9bb7c10eb
lecion <ylc931116@gmail.com>
2015-05-17 21:48:12 +0800

    优化UI细节

---
+++ app/src/main/java/com/yliec/lbs/DrawableCenterTextView.java
@@ -0,0 +1,37 @@
+package com.yliec.lbs;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by Lecion on 5/17/15.
+ */
+public class DrawableCenterTextView extends TextView{
+
+    public DrawableCenterTextView(Context context) {
+        super(context);
+    }
+
+    public DrawableCenterTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Drawable[] drawables = getCompoundDrawables();
+        if (drawables != null) {
+           Drawable drawableLeft = drawables[0];
+            if (drawableLeft != null) {
+                float textWidth = getPaint().measureText(getText().toString());
+                int drawablePadding = getCompoundDrawablePadding();
+                int drawableWidth = drawableLeft.getIntrinsicWidth();
+                float bodyWidth = textWidth + drawablePadding + drawableWidth;
+                canvas.translate((getWidth() - bodyWidth) / 2, 0);
+            }
+        }
+        super.onDraw(canvas);
+    }
+}
