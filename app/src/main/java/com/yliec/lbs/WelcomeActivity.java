0707135dad40a979356940d190d00c91d6ce3b9d
lecion <ylc931116@gmail.com>
2015-06-04 21:57:51 +0800

    增加：3s内触摸屏幕，自动进入主界面

--- app/src/main/java/com/yliec/lbs/WelcomeActivity.java
+++ app/src/main/java/com/yliec/lbs/WelcomeActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
 
 
 /**
@@ -14,16 +16,28 @@ public class WelcomeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        findViewById(R.id.splash).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                toMainActivity();
+                return true;
+            }
+        });
 
         //3秒后跳到主界面MainActivity
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(WelcomeActivity.this, MainActivity.class);
-                startActivity(i);
-                finish();
+                toMainActivity();
             }
         }, 3000);
+
+    }
+
+    private void toMainActivity() {
+        Intent i = new Intent(WelcomeActivity.this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 
 }
